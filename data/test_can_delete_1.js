de5c94574ecfc0114aaa85b9940e38168b8f74ee
AngusSbost <115364615+AngusSbost@users.noreply.github.com>
2022-10-10 21:03:30 +0100

    test

---
+++ data/test_can_delete_1.js
@@ -0,0 +1,8 @@
+var json_test_can_delete_1 = {
+"type": "FeatureCollection",
+"name": "test_can_delete_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Sgeir Eòghainn", "Source": "Mòrag Anna NicNèill", "Detaisl": "Named after a brave chap who swam ashore here from Taransay, during a clan battle - with an arrow in his thigh, according to legend." }, "geometry": { "type": "Point", "coordinates": [ -6.97487881515226, 57.865112266745548 ] } }
+]
+}
